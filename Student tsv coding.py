0fb1ddd39afc04b0ef0f76c9c411e9f6f85bcecd
teju1983 <110331829+teju1983@users.noreply.github.com>
2022-07-31 21:16:01 +0530

    Add files via upload

---
+++ Student tsv coding.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import os
+
+
+# In[10]:
+
+
+os.getcwd()
+
+
+# In[11]:
+
+
+import pandas as pd
+
+
+# In[15]:
+
+
+pd.read_csv("Student.tsv")
+
+
+# In[16]:
+
+
+pd.read_csv("Student.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
